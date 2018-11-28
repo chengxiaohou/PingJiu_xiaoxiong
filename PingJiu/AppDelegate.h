91454a9bf213dca5b90fd3e2cec125ddc6b84796
xiaoyi <2573571405@qq.com>
2018-11-28 14:06:31 +0800

    Initial Commit

---
+++ PingJiu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PingJiu
+//
+//  Created by 小熊 on 2018/11/28.
+//  Copyright © 2018年 小熊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
